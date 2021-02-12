bcda6c702827a836605fa5d9a5b4371b29674e14
VolodyaVlasov <ivlasovvladimir@gmail.com>
2021-02-13 00:04:41 +0300

    add SimpleGraph

--- src/com/company/SimpleGraph.java
+++ src/com/company/SimpleGraph.java
@@ -1,3 +1,4 @@
+package com.company;
 
 import java.util.*;
 import java.util.LinkedList;
@@ -5,10 +6,10 @@ import java.util.Queue;
 import java.util.Stack;
 
 class Vertex {
-    public int Value;
-    public boolean Hit;
-    public int index;
-    public Vertex parent;
+    protected int Value;
+    protected boolean Hit;
+    protected int index;
+    protected Vertex parent;
 
     public Vertex(int val, int i) {
         Value = val;
@@ -18,7 +19,7 @@ class Vertex {
     }
 }
 
-class SimpleGraph {
+ public class SimpleGraph {
     Vertex[] vertex;
     int[][] m_adjacency;
     int max_vertex;
@@ -30,7 +31,7 @@ class SimpleGraph {
         vertex = new Vertex[size];
     }
 
-    public void AddVertex(int value) {
+    public void addVertex(int value) {
         for (int i = 0; i < max_vertex; i++) {
             if (vertex[i] == null) {
                 vertex[i] = new Vertex(value, i);
@@ -39,7 +40,7 @@ class SimpleGraph {
         }
     }
 
-    public void RemoveVertex(int v) {
+    public void removeVertex(int v) {
         vertex[v] = null;
         for (int i = 0; i < max_vertex; i++) {
             m_adjacency[v][i] = 0;
@@ -47,30 +48,30 @@ class SimpleGraph {
         }
     }
 
-    public boolean IsEdge(int v1, int v2) {
+    public boolean isEdge(int v1, int v2) {
         return m_adjacency[v1][v2] == 1;
     }
 
-    public void AddEdge(int v1, int v2) {
+    public void addEdge(int v1, int v2) {
         m_adjacency[v1][v2] = 1;
         m_adjacency[v2][v1] = 1;
     }
 
-    public void RemoveEdge(int v1, int v2) {
+    public void removeEdge(int v1, int v2) {
         m_adjacency[v1][v2] = 0;
         m_adjacency[v2][v1] = 0;
     }
 
-    public ArrayList<Vertex> DepthFirstSearch(int VFrom, int VTo) {
+    public ArrayList<Vertex> depthFirstSearch(int VFrom, int VTo) {
         stack = new Stack<>();
         for (Vertex i : vertex) {
             i.Hit = false;
         }
-        DFS(vertex[VFrom], vertex[VTo]);
+        dfs(vertex[VFrom], vertex[VTo]);
         return new ArrayList<>(stack);
     }
 
-    public void DFS(Vertex VFrom, Vertex Vto) {
+    public void dfs(Vertex VFrom, Vertex Vto) {
         VFrom.Hit = true;
         stack.push(VFrom);
         for (int i = 0; i < max_vertex; i++) {
@@ -81,37 +82,38 @@ class SimpleGraph {
         }
         for (int i = 0; i < max_vertex; i++) {
             if (m_adjacency[VFrom.index][i] == 1 && !vertex[i].Hit) {
-                DFS(vertex[i], Vto);
+                dfs(vertex[i], Vto);
                 return;
             }
         }
         stack.pop();
         if (!stack.isEmpty()) {
-            DFS(stack.pop(), Vto);
+            dfs(stack.pop(), Vto);
         }
     }
 
 
-    public ArrayList<Vertex> BreadthFirstSearch(int VFrom, int VTo) {
+    public ArrayList<Vertex> breadthFirstSearch(int VFrom, int VTo) {
         ArrayList<Vertex> path = new ArrayList<>();
         Queue<Vertex> queue = new LinkedList<>();
+        boolean temp = true;
         queue.add(vertex[VFrom]);
         for (Vertex i : vertex) {
             i.Hit = false;
             i.parent = null;
         }
-        outerloop:
-        while (!queue.isEmpty()) {
+        while (!queue.isEmpty() && temp) {
             Vertex v = queue.poll();
             for (int i = 0; i < max_vertex; i++) {
                 if (m_adjacency[v.index][i] == 1 && !vertex[i].Hit) {
-                    Vertex temp = vertex[i];
-                    temp.parent = v;
-                    if (temp == vertex[VTo]) {
-                        break outerloop;
+                    Vertex vertex = this.vertex[i];
+                    vertex.parent = v;
+                    if (vertex == this.vertex[VTo]) {
+                        temp = false;
+                        break;
                     }
-                    temp.Hit = true;
-                    queue.add(temp);
+                    vertex.Hit = true;
+                    queue.add(vertex);
                 }
             }
             v.Hit = true;
@@ -132,7 +134,7 @@ class SimpleGraph {
         return path;
     }
 
-    public ArrayList<Vertex> WeakVertices() {
+    public ArrayList<Vertex> weakVertices() {
         ArrayList<Vertex> arrayList = new ArrayList<>();
         Vertex one;
         Vertex two;
@@ -145,7 +147,7 @@ class SimpleGraph {
                 for (int j = 0; j < max_vertex; j++) {
                     if (j != i && m_adjacency[i][j] == 1) {
                         two = vertex[j];
-                        Vertex temp = isTriangle(one, two);
+                        Vertex temp = istriangle(one, two);
                         if (temp != null) {
                             one.Hit = true;
                             two.Hit = true;
@@ -166,7 +168,7 @@ class SimpleGraph {
     }
 
 
-    public Vertex isTriangle(Vertex one, Vertex two) {
+    public Vertex istriangle(Vertex one, Vertex two) {
         for (int i = 0; i < max_vertex; i++) {
             if (m_adjacency[one.index][i] == 1 && m_adjacency[two.index][i] == 1 && m_adjacency[one.index][two.index] == 1) {
                 return vertex[i];
