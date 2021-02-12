e7a7f823e1cf937902d0a5875492385f2769e8ab
VolodyaVlasov <ivlasovvladimir@gmail.com>
2021-02-12 17:42:43 +0300

    add DynamicArray

--- src/com/company/DynamicArray.java
+++ src/com/company/DynamicArray.java
@@ -3,15 +3,14 @@ package com.company;
 import java.lang.reflect.Array;
 import java.util.Arrays;
 
-public class DynArray<T> {
+public class DynamicArray<T> {
     public T[] array;
     public int count;
     public int capacity;
     Class clazz;
 
-    public DynArray(Class clz) {
+    public DynamicArray(Class clz) {
         clazz = clz;
-
         count = 0;
         makeArray(16);
     }
@@ -30,7 +29,6 @@ public class DynArray<T> {
         } else {
             return array[index];
         }
-
     }
 
     public void append(T itm) {
@@ -39,18 +37,15 @@ public class DynArray<T> {
         }
         array[count] = itm;
         count++;
-
     }
 
     public void insert(T itm, int index) {
         if (index > count) {
             throw new ArrayIndexOutOfBoundsException("Exception: ArrayIndexOutOfBoundsException");
         }
-
         if (count >= capacity) {
             makeArray(capacity * 2);
         }
-
         if (count - index >= 0) System.arraycopy(array, index, array, index + 1, count - index);
         array[index] = itm;
         count++;
@@ -60,14 +55,11 @@ public class DynArray<T> {
         if (index >= count) {
             throw new ArrayIndexOutOfBoundsException("Exception: ArrayIndexOutOfBoundsException");
         }
-
         if (count - 1 - index >= 0) System.arraycopy(array, index + 1, array, index, count - 1 - index);
         array[count - 1] = null;
         count--;
-
         if (count < capacity / 2) {
             makeArray((int) (capacity / 1.5));
         }
     }
-
 }
